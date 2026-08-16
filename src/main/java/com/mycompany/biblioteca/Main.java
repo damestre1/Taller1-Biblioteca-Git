325cb192fb87541128a5ac0b4e336f8a06d87ad4
damestre <diegomestre98@gmail.com>
2026-08-16 17:05:14 -0500

    feat: preparar estructura inicial de Main para Customer

---
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -0,0 +1,16 @@
+package com.mycompany.biblioteca;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static ArrayList<Customer> customers = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Aquí irá el menú (Fase 8)
+
+    }
+}
